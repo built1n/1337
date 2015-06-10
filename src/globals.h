686136814ead78ada29b750a4b92f116fb4c97e9
Franklin Wei <git@fwei.tk>
2015-06-09 20:34:16 -0400

    text drawing

--- src/globals.h
+++ src/globals.h
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 
 #include <zlib.h>
 
--- src/util.c
+++ src/util.c
@@ -12,3 +12,32 @@ void fatal(const char *fmt, ...)
 
     exit(EXIT_FAILURE);
 }
+
+
+void putsxy(SDL_Renderer *rend, int x, int y, const char *fmt, ...)
+{
+    static TTF_Font *font = NULL;
+    va_list ap;
+    va_start(ap, fmt);
+    char buf[256];
+    vsnprintf(buf, sizeof(buf), fmt, ap);
+    va_end(ap);
+
+    if(!font)
+    {
+        font = TTF_OpenFont("LiberationMono-Regular.ttf", 16);
+
+        if(!font)
+            fatal("failed to open font: %s", TTF_GetError());
+    }
+
+    const SDL_Color color = { 0xff, 0xff, 0xff, 0xff };
+    SDL_Surface *text = TTF_RenderText_Solid(font, buf, color);
+
+    SDL_Texture *t = SDL_CreateTextureFromSurface(rend, text);
+    SDL_Rect dest = { x, y, text->w, text->h };
+    SDL_RenderCopy(rend, t, NULL, &dest);
+
+    SDL_FreeSurface(text);
+    SDL_DestroyTexture(t);
+}
